386b0ca64008475ff8bcfdc6bd02ea7f1c14fd9d
Oswaldo Garcia <oswaldo.gs.developer@gmail.com>
2023-04-20 14:35:36 -0600

    Feat(3): Enums

---
+++ src/01-enum.ts
@@ -0,0 +1,17 @@
+enum ROLES {
+  ADMIN = "admin",
+  SELLER = "seller",
+  CUSTOMER = "customer",
+ }
+
+type User = {
+  username: string;
+  role: ROLES;
+}
+
+const oswaUser: User = {
+  username: "oswabytes",
+  role: ROLES.ADMIN
+}
+
+
