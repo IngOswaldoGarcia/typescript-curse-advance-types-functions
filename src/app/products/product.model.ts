d2ff2a9a00195b33eb79e98f5d1266deef3bc737
Oswaldo Garcia <oswaldo.gs.developer@gmail.com>
2023-04-24 21:10:56 -0600

    Feat(16): ejemplo de CRUD

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,11 +1,18 @@
 import {Category} from './categories/category.model';
-import { BaseModel } from './../base.model'
+import { BaseModel } from './../base.model';
+
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
 
 
 export interface Product extends BaseModel {
   title: string;
+  image: string;
+  description: string;
   stock: number;
   sizes?: Sizes;
+  color: string;
+  price: number;
   category: Category;
+  isNew: boolean;
+  tags: string[];
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,16 @@ export const products: Product[] = [];
 export const  addProduct = (data: Product) => {
   products.push(data);
 }
+
+
+export const  updateProduct = (id: string, changes: Product) => {
+  //code
+}
+
+export const  deleteProduct = (id: string) => {
+  //code
+}
+
+export const  getProduct = (id: string) => {
+  //code
+}
